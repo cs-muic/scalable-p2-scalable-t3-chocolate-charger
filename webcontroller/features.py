960177f9692f5fc10a524ec87843f58a7bda48d0
marcArisen <markjingnum9@gmail.com>
2022-06-06 23:44:28 +0700

    testing workflow

--- webcontroller/features.py
+++ webcontroller/features.py
@@ -1,1 +1,21 @@
-import app
+from minio import Minio
+from minio.error import S3Error
+
+
+def upload_video(file):
+    client = Minio(
+        "localhost:7000",
+        access_key="minio",
+        secret_key="minio123",
+        secure= False
+    )
+
+    found = client.bucket_exists("video")
+    if not found:
+        client.make_bucket("video")
+    else:
+        print("Bucket 'video' already exists")
+
+    client.fput_object(
+        "video", "temp.mp4", file
+    )
