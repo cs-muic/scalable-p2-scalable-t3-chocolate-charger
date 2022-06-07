c8f3053cf4e0f29ed83770564e19db393cc27f34
marcArisen <markjingnum9@gmail.com>
2022-06-07 22:19:58 +0700

    minio can download obj

--- webcontroller/app.py
+++ webcontroller/app.py
@@ -2,20 +2,21 @@ import os
 from flask import Flask, request, jsonify, render_template, abort
 from flask_caching import Cache
 from flask_minio import Minio
-from features import temp, upload_video
+from features import *
 from rq.job import Job
 import redis
 from rq import Queue
 
 
-
-
 app = Flask(__name__)
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
-REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = os.getenv("REDIS_PORT", "6379")
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "")
+
+
+minio = minioController()
 
 redis_connection = redis.Redis(
     host=REDIS_HOST,
@@ -28,6 +29,7 @@ redis_queue = Queue(connection=redis_connection)
 @app.route("/enqueue", methods=["POST"])
 def enqueue():
     job = redis_queue.enqueue(temp)
+    # return jsonify({"job_id": "a"})
     return jsonify({"job_id": job.id})
 
 @app.route('/api/submit', methods=['POST'])
@@ -44,14 +46,23 @@ def list():
 @app.route('/api/upload', methods=['POST'])
 def upload():
     path = request.json.get("path", None)
-    upload_video(path)
+    minio.upload_video(path)
     return jsonify({"OK": "DONE"}), 200
 
-@app.route('/api/upload', methods=['POST'])
-def upload():
-    path = request.json.get("path", None)
-    upload_video(path)
-    return jsonify({"OK": "DONE"}), 200
+@app.route('/api/listbucket', methods=['POST'])
+def listing_buckets():
+    # path = request.json.get("path", None)
+    minio.list_buckets()
+    return jsonify({"OK": "listing"}), 200
+
+@app.route('/api/listobject', methods=['POST'])
+def listing_object():
+    # path = request.json.get("path", None)
+    minio.download_objects("video")
+    return jsonify({"OK": "listing"}), 200
+
+
+
 
 
 if __name__ == '__main__':
--- webcontroller/features.py
+++ webcontroller/features.py
@@ -1,8 +1,57 @@
+from xmlrpc.client import ResponseError
 from minio import Minio
 from minio.error import S3Error
 
 import time
 from rq import get_current_job
+
+class minioController:
+    def __init__(self):
+        self.client = Minio(
+        "localhost:9000",
+        access_key="minio",
+        secret_key="minio123",
+        secure= False
+    )
+
+    def upload_video(self, file):
+        found = self.client.bucket_exists("video")
+        if not found:
+            self.client.make_bucket("video")
+        else:
+            print("Bucket 'video' already exists")
+
+        # TODO: change this "temp.mp4"
+        self.client.fput_object(
+            "video", "temp.mp4", file
+        )
+    
+    def list_buckets(self):
+        buckets = self.client.list_buckets()
+        for bucket in buckets:
+            print(bucket.name, bucket.creation_date)
+            print("==================")
+            print(bucket)
+            print("==================")
+    
+    def download_objects(self, bucketname):
+        objs = self.client.list_objects(bucketname)
+        for obj in objs:
+            print(obj.object_name)
+            self.client.fget_object(bucketname, obj.object_name, "./download/kenny.mp4", request_headers=None)
+            # try:
+            #     response = self.client.get_object(bucketname, obj)
+            #     # Read data from response.
+            # finally:
+            #     response.close()
+            #     response.release_conn()
+            # try:
+            #     print(self.client.fget_object(bucketname, obj, './download/'))
+            # except ResponseError as err:
+            #     print(err)
+
+    def get_file(self, bucket, object, name):
+        self.client.fget_object(bucket, object, name)
 
 def temp():
     job = get_current_job()
@@ -12,20 +61,5 @@ def temp():
         "job_id": job.id
     }
 
-def upload_video(file):
-    client = Minio(
-        "localhost:7000",
-        access_key="minio",
-        secret_key="minio123",
-        secure= False
-    )
 
-    found = client.bucket_exists("video")
-    if not found:
-        client.make_bucket("video")
-    else:
-        print("Bucket 'video' already exists")
-
-    client.fput_object(
-        "video", "temp.mp4", file
-    )
+# def get_object(bucket, object, name):
