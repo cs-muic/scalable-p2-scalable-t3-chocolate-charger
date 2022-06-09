373f76ad8e5e44b8f6ebf68312c897ef4565f0ae
marcArisen <markjingnum9@gmail.com>
2022-06-09 22:10:37 +0700

    updated status ID

--- webcontroller/app.py
+++ webcontroller/app.py
@@ -16,9 +16,9 @@ import json
 app = Flask(__name__)
 MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
 MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
-# REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-# REDIS_PORT = os.getenv("REDIS_PORT", "6379")
-# REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", "")
+
+# job id counter
+init_job_id = 0
 
 class Pocket:
     def __init__(self, filename, path, minio):
@@ -26,15 +26,6 @@ class Pocket:
         self.path = path
         self.jobId = -1 # before entering the queue
         self.minio = minio
-    
-
-# CHECKING REDIS CONNECTION, DON'T DELETE
-# try:
-#     response = redis_connection.client_list()
-#     print(response)
-# except redis.ConnectionError:
-#     #your error handlig code here 
-#     print("error")
 
 @app.route('/api/make_gif', methods=['POST'])
 def make_gif():
@@ -48,10 +39,25 @@ def make_gif():
 
     return jsonify({"job": job_worker2.id}), 200
 
+@app.route('/api/make_gif2', methods=['POST'])
+def make_gif2():
+    global init_job_id
+    init_job_id += 1
+    redis_conn.set(init_job_id, "Extracting Frames")
+
+    uploaded_path = request.json.get("path", None)
+    uploaded_filename = request.json.get("filename", None)
+    pocket = Pocket(filename=uploaded_filename, path=uploaded_path, minio=minio)
+    minio.upload_video(pocket.path, pocket.filename)
+    job_worker1 = extract_queue.enqueue(frames_extraction, pocket.filename, init_job_id)
+    return jsonify({"jobId": init_job_id}), 200
+
 @app.route('/api/status', methods=['POST'])
 def check_status():
     job_id = request.json.get("jobId", None)
-    return jsonify({job_id: Job.fetch(job_id, connection=redis_conn).get_status()}), 200
+    process = redis_conn.get(job_id)
+    print(str(process))
+    return jsonify({"process": str(process)}), 200
     
 
 # api that return a list of buckets (name)
--- webcontroller/features.py
+++ webcontroller/features.py
@@ -4,37 +4,45 @@ import time
 from rq import get_current_job
 from minioController import minio
 import subprocess
+from redisConnection import redis_conn, extract_queue, compose_queue
 
-def notify_queue(str):
-    print(str)
-
-def frames_extraction(filename): 
-
+def frames_extraction(filename, workId): 
     job = get_current_job()
-
+    
+    # pull video from minio
     minio.download_video(filename)
     path = str.split(filename, '.')[0]
     print(path)
-    #####
+
     process = subprocess.Popen(f'sh /Users/marcmarkcat/Desktop/Study/scalable/P2/scalable-p2-scalable-t3-chocolate-charger/scripts/extract.sh ./temp/{filename} frames', shell=True, stdout=subprocess.PIPE)
     process.wait()
-    #####
-    # os.popen(f'sh /Users/marcmarkcat/Desktop/Study/scalable/P2/scalable-p2-scalable-t3-chocolate-charger/scripts/extract.sh ./temp/{filename} frames') # TOFIX: harcode and path
+
+    # upload frames back to minio
     minio.upload_folder("./frames", job.id)
-    print("work done")
-    return path
+    print(f"extraction {job.id} done")
+
+    #update state of the job
+    redis_conn.set(workId, "Extracted >> composing")
+
+    job_worker2 = compose_queue.enqueue(image_compose, job.id, workId)
 
 
-def image_compose(jobId):
+def image_compose(jobId, workId):
+
     #download all frames
-    print("debugging")
+    print(f"Start Composing {jobId}")
     minio.download_extracted_frames(jobId)
-    #####
+
     process = subprocess.Popen(f'sh /Users/marcmarkcat/Desktop/Study/scalable/P2/scalable-p2-scalable-t3-chocolate-charger/scripts/compose.sh ./download/{jobId} output.gif', shell=True, stdout=subprocess.PIPE)
     process.wait()
-    #####
-    # os.popen(f'sh /Users/marcmarkcat/Desktop/Study/scalable/P2/scalable-p2-scalable-t3-chocolate-charger/scripts/compose.sh ./temp/{filename} frames') # TOFIX: harcode and path
+    print(f"Done {jobId}")
+    redis_conn.set(workId, "Job Completd")
 
+def update_status(worker, workId):
+    if worker == 1:
+        redis_conn.set(workId, "Extracted >> composing")
+    else:
+        redis_conn.set(workId, "Job Completd")
 
 
 def some_long_function(some_input):
