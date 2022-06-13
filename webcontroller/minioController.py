07f5dc317c605654c0b30712308a5e1457c70052
Philippe Sagon <Philipee.sagon44@gmail.com>
2022-06-13 19:11:16 +0700

    CORS

--- webcontroller/minioController.py
+++ webcontroller/minioController.py
@@ -75,7 +75,7 @@ class minioController:
         if not found:
             self.client.make_bucket(bucketname)
         else:
-            print(f"Bucket '{bucketname}' already exists")
+            print(f"Bucket '{bucketname}' already exists")        
         objs = self.client.list_objects(bucketname)
         lst = [obj.object_name for obj in objs]
         return lst
